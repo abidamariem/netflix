2f794de22f284c0708c8a521f5ad6cf72b396ce0
abidamariem <147394085+abidamariem@users.noreply.github.com>
2023-12-25 11:41:00 +0100

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,56 @@
+import ListMovie from './component/ListMovie'
+import './App.css'
+import moviesData from "./movies.json";
+import { useState } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Container from 'react-bootstrap/Container';
+import Navbar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav';
+import logo from './logoNetflix.png'
+import AddMovie from './component/AddMovie';
+import Search from './component/Search';
+
+function App() {
+  const [searchMovie,setSearchMovie]=useState("");
+  const [searchRating,setSearchRating]=useState("");
+
+  const [movies,setMovies]=useState(moviesData);
+  const handleAdd=(newMovie)=>{
+    setMovies([...movies,newMovie])
+  }
+  console.log(movies)
+  const search =(name)=>{
+    setSearchMovie(name);
+  }
+  const searchRatingg =(name)=>{
+    setSearchRating(name);
+  }
+  return (
+    <>
+    <Navbar bg="dark" data-bs-theme="dark">
+        <Container style={{backgroundColor:"black"}}>
+          <Navbar.Brand href="#app">
+            <img
+              alt=""
+              src={logo}
+              width="30"
+              height="30"
+              className="d-inline-block align-top"
+            />{' '}Netflix</Navbar.Brand>
+          <Nav className="me-auto">
+            <Nav.Link href="#app">Liste</Nav.Link>
+            <Nav.Link href="#addMovie">Ajouter</Nav.Link>
+          </Nav>
+          <Search search={search} searchRating={searchRatingg}/>
+
+        </Container>
+      </Navbar>
+      <AddMovie handleAdd={handleAdd}/>
+
+    <ListMovie movies={movies.filter((movie)=> movie.Title.toLocaleUpperCase().includes(searchMovie.toLocaleUpperCase().trim()) && movie.imdbRating>=searchRating)} />
+
+    </>
+  );
+}
+
+export default App;
